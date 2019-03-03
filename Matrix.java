d32fba414528a815560f44441e46d354932e3e6b
phobossky8 <labotski.job@gmail.com>
2019-03-04 01:24:49 +0300

    HW3 updated and finished

--- Matrix.java
+++ Matrix.java
@@ -1,5 +1,3 @@
-package by.ldu.task.ex2;
-
 import java.math.BigDecimal;
 import java.util.Random;
 import java.util.Scanner;
@@ -11,20 +9,46 @@ public class Matrix {
 
 		findMaxElement(arr);
 		findMinElement(arr);
+		System.out.println("----------------------------------");
 		averageValues(arr);
+		System.out.println("----------------------------------");
+		findLocalMax(arr);
+		findLocalMin(arr);
 		System.out.println("----------------------------------");
 		transposeMatrix(arr);
 	}
 
 	public static double[][] initRandomMatrix() {
-		Scanner in = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
+		byte N = 0, M = 0;
 		System.out.print("Enter first size of Matrix, N:");
-		byte N = in.nextByte();
+		do {
+			if (scanner.hasNextByte()) {
+				N = scanner.nextByte();
+				if (N <= 0) {
+					System.out.println("Incorrect input. Try again: ");
+				}
+			} else {
+				System.out.println("Incorrect input. Try again: ");
+				scanner.next();
+			}
+		} while (N <= 0);
+
 		System.out.print("Enter second size of Matrix, M:");
-		byte M = in.nextByte();		
-		
+		do {
+			if (scanner.hasNextByte()) {
+				M = scanner.nextByte();
+				if (M <= 0) {
+					System.out.println("Incorrect input. Try again: ");
+				}
+			} else {
+				System.out.println("Incorrect input. Try again: ");
+				scanner.next();
+			}
+		} while (M <= 0);
+
 		Random rand = new Random();
-		
+
 		double[][] arr = new double[N][M];
 
 		for (int i = 0; i < arr.length; i++) {
@@ -48,7 +72,7 @@ public class Matrix {
 				}
 			}
 		}
-		System.out.println(max);
+		System.out.println("MAX element of matrix is: " + max);
 	}
 
 	// finding minimum element of Matrix
@@ -61,28 +85,30 @@ public class Matrix {
 				}
 			}
 		}
-		System.out.println(min);
+		System.out.println("MIN element of matrix is: " + min);
 	}
-	
-	//finding arithmetical and geometric averages
+
+	// finding arithmetical and geometric averages
 	public static void averageValues(double[][] array) {
-        int avg = 0;
-        int count = 0;
-        BigDecimal big = new BigDecimal(1);
+		int avg = 0;
+		int count = 0;
+		BigDecimal geom = new BigDecimal(1);
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-                avg += array[i][j];
-                big = big.multiply(BigDecimal.valueOf(array[i][j]));
-                count++;
-            }
-        }
+		for (int i = 0; i < array.length; i++) {
+			for (int j = 0; j < array[i].length; j++) {
+				avg += array[i][j];
+				geom = geom.multiply(BigDecimal.valueOf(array[i][j]));
+				count++;
+			}
+		}
+		System.out.println("Arithmetical average is :" + (double) avg / count);
+		System.out.println("Geometric average is: " + Math.pow(geom.doubleValue(), 1.0 / count));
+	}
 
-        System.out.println("Arithmetical average is :" + (double) avg / count);
-        System.out.println("Geometric average is: " + Math.pow(big.doubleValue(), 1.0 / count));
-}
-
-	// transposing Matrix
+	// transposing Matrix. This method working and working correctly only with
+	// Matrix, who has same number of lines and columns.
+	// (according to our task, which says that we shouldn't use additional memory
+	// (for creating temporary variables or other arrays)
 	public static void transposeMatrix(double[][] arr) {
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0 + i; j < arr[i].length; j++) {
@@ -98,4 +124,112 @@ public class Matrix {
 			System.out.println();
 		}
 	}
+
+	// finding of first local minimum of Matrix
+	public static void findLocalMin(double[][] array) {
+		boolean isLocalMinFound = false;
+
+		for (int j = 1; j < array[0].length - 1; j++) {
+			if (array[0][j - 1] > array[0][j] && array[0][j + 1] > array[0][j] && array[1][j] > array[0][j]) {
+				isLocalMinFound = true;
+				System.out.println("Position of Local MIN is: [0][" + j + "]");
+				break;
+			}
+		}
+		if (!isLocalMinFound && array.length > 2) {
+			for (int i = 1; i < array.length - 1; i++) {
+				if (array[i - 1][0] > array[i][0] && array[i + 1][0] > array[i][0] && array[i][1] > array[i][0]) {
+					isLocalMinFound = true;
+				}
+				if (!isLocalMinFound) {
+					for (int j = 1; j < array[i].length - 1; j++) {
+						if (array[i - 1][j] > array[i][j] && array[i + 1][j] > array[i][j]
+								&& array[i][j - 1] > array[i][j] && array[i][j + 1] > array[i][j]) {
+							System.out.println("Position of Local MIN is: [" + i + "][" + j + "]");
+							isLocalMinFound = true;
+							break;
+						}
+					}
+				}
+				if (!isLocalMinFound) {
+					if (array[i - 1][array[i].length - 1] > array[i][array[i].length - 1]
+							&& array[i + 1][array[i].length - 1] > array[i][array[i].length - 1]
+							&& array[i][array[i].length - 2] > array[i][array[i].length - 1]) {
+						isLocalMinFound = true;
+					}
+				}
+			}
+		}
+		if (!isLocalMinFound) {
+			for (int j = 1; j < array[array.length - 1].length - 1; j++) {
+				if (array[array.length - 1][j - 1] > array[array.length - 1][j]
+						&& array[array.length - 1][j + 1] > array[array.length - 1][j]
+						&& array[array.length - 2][j] > array[array.length - 1][j]) {
+					isLocalMinFound = true;
+					System.out.println("Position of Local MIN is: [" + (array.length - 1) + "][" + j + "]");
+					break;
+				}
+
+			}
+		}
+
+		if (!isLocalMinFound) {
+			System.out.println("Local min = -1");
+		}
+	}
+
+	// finding of first local maximum of Matrix
+	public static void findLocalMax(double[][] array) {
+		boolean isLocalMaxFound = false;
+
+		for (int j = 1; j < array[0].length - 1; j++) {
+			if (array[0][j - 1] < array[0][j] && array[0][j + 1] < array[0][j] && array[1][j] < array[0][j]) {
+				isLocalMaxFound = true;
+				System.out.println("Position of Local MAX is: [0][" + j + "]");
+				break;
+			}
+		}
+		if (!isLocalMaxFound && array.length > 2) {
+			for (int i = 1; i < array.length - 1; i++) {
+				if (array[i - 1][0] < array[i][0] && array[i + 1][0] < array[i][0] && array[i][1] < array[i][0]) {
+					isLocalMaxFound = true;
+				}
+				if (!isLocalMaxFound) {
+					for (int j = 1; j < array[i].length - 1; j++) {
+						if (array[i - 1][j] < array[i][j] && array[i + 1][j] < array[i][j]
+								&& array[i][j - 1] < array[i][j] && array[i][j + 1] < array[i][j]) {
+							System.out.println("Position of Local MAX is: [" + i + "][" + j + "]");
+							isLocalMaxFound = true;
+							break;
+						}
+					}
+				}
+				if (!isLocalMaxFound) {
+					if (array[i - 1][array[i].length - 1] < array[i][array[i].length - 1]
+							&& array[i + 1][array[i].length - 1] < array[i][array[i].length - 1]
+							&& array[i][array[i].length - 2] < array[i][array[i].length - 1]) {
+						isLocalMaxFound = true;
+					}
+				}
+			}
+		}
+		if (!isLocalMaxFound) {
+			for (int j = 1; j < array[array.length - 1].length - 1; j++) {
+				if (array[array.length - 1][j - 1] < array[array.length - 1][j]
+						&& array[array.length - 1][j + 1] < array[array.length - 1][j]
+						&& array[array.length - 2][j] < array[array.length - 1][j]) {
+					isLocalMaxFound = true;
+					System.out.println("Position of Local MAX is: [" + (array.length - 1) + "][" + j + "]");
+					break;
+				}
+
+			}
+		}
+
+		if (!isLocalMaxFound) {
+			System.out.println("Local max is -1");
+		}
+
+	}
+
 }
